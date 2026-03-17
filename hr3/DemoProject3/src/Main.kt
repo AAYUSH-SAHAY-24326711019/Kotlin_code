92498a8371e31bb264a204ddcb7239fa5a1593c8
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-03-17 14:00:31 +0530

    savepoint hr 3

---
+++ hr3/DemoProject3/src/Main.kt
@@ -0,0 +1,71 @@
+fun main(){
+    //take input from a user, number
+
+//    println("Enter a number")
+//    val num1 : Int = readln().toInt()
+
+//    if(num1>0)
+//        println("+ve number")
+//    else if (num1==0)
+//        println("number is 0")
+//    else
+//        println("number is -ve")
+
+//    val a : Int =5
+//    val b : Int =10
+//    val c : Int =a+b
+//    println("Value result = ${c}")
+
+//    val max = if(a>b){a} else{b}
+//    println("value is : ${max}")
+
+
+//    val marks:Int = readln().toInt()
+//    val max:Char = when(marks)
+//    {
+//        in 91..100 ->'A'
+//        in 81..90->'B'
+//        else ->'F'
+//    }
+//    println("value is : ${max}")
+//
+
+
+//var flag:Boolean = false
+//    while(!flag==true){
+//        var inp:Boolean = readln().toBoolean()
+//
+//        println(" _ .")
+//
+//        if(inp == false)
+//            flag=false
+//    }
+
+//var v:Int = 0
+//    do{
+//        println(v)
+//        v++
+//    }while (v<=10);
+
+    /*for (i in 1..10){
+        print("$i ")
+    }
+    for (i in 10 downTo 1 step 2){
+        print("$i ")
+    }*/
+
+//    val arr : Array<String> = arrayOf("abc","aabbcc","aaabbbccc")
+//
+//    var c : Int = 0
+//    for (a in arr.reversed()){
+//        println(" ".repeat(c)+a)
+//        c = c+1
+//
+//    }
+
+
+
+
+
+
+}
