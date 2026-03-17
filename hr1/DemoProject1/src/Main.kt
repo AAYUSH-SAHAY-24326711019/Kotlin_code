9d36ecad850afcd4a53a51be3659bd422fce2408
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-03-17 09:13:53 +0530

    save point hr 1

---
+++ hr1/DemoProject1/src/Main.kt
@@ -0,0 +1,32 @@
+fun main(){
+    //1 print and println (uncomment the below 2 lines)
+   // println("Hello World 11")
+   // print(" hi this is the print message")
+
+    //2. variables in kotlin
+
+    //module 1 (uncomment from start to end)
+    //start
+
+//    val name : String= "Aayush Sahay"
+//    print("Hello $name") //this is interpolation using templates
+//    //prevent using the concatenation using java
+//
+//    var a =10
+//    var b=20
+//    println("Values of a = $a and b = $b , sum = ${a+b}")
+//    a=100
+//    b=100
+//    println("Values of a = $a and b = $b , sum = ${a+b}")
+
+    //end
+
+    //module 2 (uncomment from the start to the end)
+    //start
+
+    var firstName:String = "Aayush"
+    var lastName:String = "Sahay"
+    println("First name : $firstName\nLast name  : $lastName")
+
+    //end
+}
